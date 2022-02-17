604cce47da439219eded3553c189e2f70a520712
IT21281396 <99307558+IT21281396@users.noreply.github.com>
2022-02-17 12:56:36 +0000

    Completed

--- tute02.cpp
+++ tute02.cpp
@@ -12,33 +12,36 @@ Type = 3
 OtRate = 1700
 Please Note that the input command in C++ is std::cin. This is a representation of the Keyboard.*/
 
-#include <stdio.h>
-int main()
+#include <iostream>
+
+long Factorial(int no);
+long nCr(int n, int r);
+
+int main() {
+  int n, r;
+  std::cout << "Enter a value for n ";
+  std::cin >> n;
+  std::cout << "Enter a value for r ";
+  std::cin >> r;
+  std::cout << "nCr = ";
+  std::cout << nCr(n,r);
+  std::cout << std::endl;
+  return 0;
+}
+long Factorial(int no)
 {
-   double salary, netSalary;
-   int etype, otHrs, otRate;
-   printf("Enter Employee Type : ");
-   scanf("%d", &etype);
-   printf("Enter Salary  : ");
-   scanf("%f", &salary);
-   printf("Enter OtHrs : ");
-   scanf("%d", &otHrs); 
-   
-   switch (etype) {
-      case 1 :
-          otRate = 1000;
-          break;
-      case 2 :
-          otRate = 1500;
-          break;
-      default :
-          otRate = 1700;
-          break;
-   }
-
-
-   netSalary = salary + otHrs* otRate;
-   printf("Net Salary is %f ", netSalary);
-  
-   return 0;
+  long fac;
+  int i;
+  fac=1;
+  for(i=no;i>=1;i--)
+    {
+      fac=fac*i;
+    }
+  return fac;
+}
+long nCr(int n, int r)
+{
+  long nCr;
+  nCr=Factorial(n)/Factorial(r)*Factorial(n-r);
+  return nCr;
 }
